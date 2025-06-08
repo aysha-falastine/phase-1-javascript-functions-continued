fed26ef3b4fbb4c61ed54ed4bdd4d1e77f995eb0
asha mohamed <falastine924@gmail.com>
2025-06-08 21:02:04 +0300

    index.html

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// index.js
+
+// Function declaration with default parameter
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function expression with default parameter
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// Function that returns another function 
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  }
+}
